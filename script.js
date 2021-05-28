06727a72bae263e1ec8d94e854f434be1828a51c
rkdudley <rattikarndudley@gmail.com>
2021-05-28 12:21:42 -0400

    add js script for sticky bar

---
+++ script.js
@@ -0,0 +1,9 @@
+const navbar = document.getElementById('main-nav');
+
+window.onscroll = () => {
+    if (window.scrollY > 100) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
